f9d29158f2c7128095dbc27c2d3a7f935685f056
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-07-03 20:09:44 -0600

    Test these

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,29 @@ exports.createPages = async ({ graphql, actions }) => {
     const { createRedirect } = actions
 
     createRedirect({
-        fromPath: `/rsvp`,
-        toPath: `https://d9cz8f9nzru.typeform.com/to/ZjBld0ay`,
+        fromPath: `/rsvp_camp`,
+        toPath: `https://d9cz8f9nzru.typeform.com/to/ZlSu9k3G`,
     })
+
+    createRedirect({
+        fromPath: `/rsvp_camp_plus_one`,
+        toPath: `https://d9cz8f9nzru.typeform.com/to/LK7aqUZv`,
+    })
+
+    createRedirect({
+        fromPath: `/rsvp`,
+        toPath: `https://d9cz8f9nzru.typeform.com/to/vth5GJHQ`,
+    })
+
+    createRedirect({
+        fromPath: `/rsvp_plus_one`,
+        toPath: `https://d9cz8f9nzru.typeform.com/to/Kt3IVVHB`,
+    })
+
+    createRedirect({
+        fromPath: `/rsvp_family`,
+        toPath: `https://d9cz8f9nzru.typeform.com/to/oedVdRTw`,
+    })
+
+
 }
